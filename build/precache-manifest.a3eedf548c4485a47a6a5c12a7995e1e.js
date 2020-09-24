bb38b6e4e90c653d55f7c106d58f31e508f3c46b
Eira Garrett <eira.garrett@gmail.com>
2020-09-24 12:11:11 -0400

    Updated build for heroku

---
+++ build/precache-manifest.a3eedf548c4485a47a6a5c12a7995e1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad8fab6e80239f0a931b51b1648d31c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e940285371d14dd7d723",
+    "url": "/static/css/main.49780a4d.chunk.css"
+  },
+  {
+    "revision": "e4e109465e8f114e1741",
+    "url": "/static/js/2.6746cc99.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.6746cc99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e940285371d14dd7d723",
+    "url": "/static/js/main.90d08ccf.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  }
+]);
